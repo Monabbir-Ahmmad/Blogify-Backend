373f6930e474c1f85b346617d8b98743aa147620
Monabbir Ahmmad Tajbir <monabbir.ahmmad@gmail.com>
2023-05-03 18:10:55 +0600

    :)

--- backend/src/routes/test.route.js
+++ backend/src/routes/test.route.js
@@ -1,21 +1,7 @@
 import express from "express";
-import { UserResDto } from "../dtos/response/user.res.dto.js";
 
 export const testRouter = express.Router();
 
 testRouter.route("/").get((req, res) => {
-  const userList = [
-    new UserResDto({
-      id: 1,
-      password: "123456",
-      name: "John Doe",
-    }),
-    new UserResDto({
-      id: 2,
-      password: "123456",
-      name: "Jane Doe",
-    }),
-  ];
-
-  res.status(200).send(userList);
+  res.send("Test route");
 });
